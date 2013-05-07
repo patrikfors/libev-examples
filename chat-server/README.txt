1dd4946bbb6ddd0e30bce53a79d974ee982906cf
Patrik Fors <patrik.fors@gmail.com>
2013-05-07 21:41:32 +0200

    Updated README, formatted code. Probably wont work since I didnt' test before comitting.

--- chat-server/README.txt
+++ chat-server/README.txt
@@ -1,18 +1,9 @@
-A simple chat server using libevent.
+A simple chat server using libev and winsock2.
 -------------------------------------------------------------------------------
 
-Requires libevent 2.0+.
+Based on the libevent example from https://github.com/jasonish/libevent-examples.
 
-First you must download and build libevent, you do not need to install
-it.  
-
-    libevent homepage: http://www.monkey.org/~provos/libevent/
-
-To build libevent_echosrv:
-
-    LIBEVENT=~/src/libevent-2.0.12-stable make
-
-where LIBEVENT points to the location of your built libevent.
+Libev 4.15 is embedded in the project.
 
 USAGE
 
@@ -23,6 +14,3 @@ USAGE
     Then telnet to localhost port 5555 from multiple terminals.
     Anything you type in one terminal will be sent to the other
     connected terminals.
-
---
-Jason Ish <ish@unx.ca>
--- chat-server/chat-server.c
+++ chat-server/chat-server.c
@@ -1,8 +1,10 @@
-/* 
+/*
  * The chat server example from libevent rewritten for libev and winsock
  */
 
 /*
+ * License notice from the libevent example this was based on.
+ * 
  * Copyright (c) 2011, Jason Ish
  * All rights reserved.
  *
@@ -17,7 +19,7 @@
  *   notice, this list of conditions and the following disclaimer in
  *   the documentation and/or other materials provided with the
  *   distribution.
- *     
+ *
  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -38,7 +40,7 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #include <WinSock2.h>
-#include <WS2tcpip.h> // socklen_t
+#include <WS2tcpip.h> /* socklen_t */
 
 /* Need to link with Ws2_32.lib */
 #pragma comment(lib, "ws2_32.lib")
@@ -61,22 +63,27 @@
 /* Port to listen on. */
 #define SERVER_PORT 5555
 
-/**
+/*
  * A struct for client specific data.
  *
  * This also includes the tailq entry item so this struct can become a
  * member of a tailq - the linked list of all connected clients.
  */
 struct client_t {
-	/* 
+	/*
 	* This struct wraps the io watcher.
-	* The watcher has to be first entry in the struct to be able to 
+	* The watcher has to be first entry in the struct to be able to
 	* cast from client_t to ev_io
 	*/
-	ev_io io; 
+	ev_io io;
 
-	/* 
-	* The clients socket, really only needed with Winsock 
+	/*
+	 * It is a bit of a pain to use libev with winsock since libev requires
+	 * file descriptors while all of winsock's socket functions requires
+	 * sockets to be of type SOCKET. On real OS's they are just the same.
+	 * To work around that, the SOCKET socket is stored in the io wrappers
+	 * for both client and server. The file descriptor that libev uses is
+	 * stored in the ev_io struct.
 	*/
 	SOCKET socketd;
 
@@ -88,23 +95,24 @@ struct client_t {
 };
 
 struct server_t {
-	/* 
+	/*
 	* This struct wraps the io watcher.
-	* The watcher has to be first entry in the struct to be able to 
+	* The watcher has to be first entry in the struct to be able to
 	* cast from client_t to ev_io
 	*/
-	ev_io io; 
+	ev_io io;
 
-	/* 
-	* The clients socket, really only needed with Winsock 
+	/*
+	* The SOCKET socket.
 	*/
 	SOCKET socketd;
 };
-/**
+
+/*
  * The head of our tailq of all connected clients.  This is what will
  * be iterated to send a received message to all connected clients.
  */
-TAILQ_HEAD(, client_t) client_tailq_head;
+TAILQ_HEAD (, client_t) client_tailq_head;
 
 /*
 * perror for winsock errors
@@ -112,13 +120,14 @@ TAILQ_HEAD(, client_t) client_tailq_head;
 void socket_perror (char const* message)
 {
 	char* buffer;
-	FormatMessageA (FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+	FormatMessageA (FORMAT_MESSAGE_ALLOCATE_BUFFER |
+	                FORMAT_MESSAGE_FROM_SYSTEM,
 	                0, WSAGetLastError(), 0, (char*) &buffer, 0, 0);
 	fprintf (stderr, "%s: %s\n", message, buffer);
 	LocalFree (buffer);
 }
 
-/**
+/*
  * Called by libev when there is data to read.
  */
 void read_cb (struct ev_loop* loop, struct ev_io* watcher, int revents)
@@ -131,8 +140,11 @@ void read_cb (struct ev_loop* loop, struct ev_io* watcher, int revents)
 
 	/* Read 8k at a time and send it to all connected clients. */
 	for (;;) {
-		// Receive message from client socket
-		read = recv (this_client->socketd, (char*) buffer, _countof (buffer), 0);
+		/* Receive message from client socket */
+		read = recv (this_client->socketd,
+		             (char*) buffer,
+		             _countof (buffer),
+		             0);
 
 		if (read < 0) {
 			if (WSAEWOULDBLOCK ==  WSAGetLastError()) {
@@ -145,23 +157,28 @@ void read_cb (struct ev_loop* loop, struct ev_io* watcher, int revents)
 			ev_io_stop (loop, &this_client->io);
 
 			/* Remove the client from the tailq. */
-			TAILQ_REMOVE (&client_tailq_head, this_client, entries);
+			TAILQ_REMOVE (&client_tailq_head, 
+					this_client, 
+					entries);
 
 			closesocket (this_client->socketd);
 			free (this_client);
 			return;
 		}
 
-			
+
 		TAILQ_FOREACH (client, &client_tailq_head, entries) {
 			if (client != this_client) {
-				send (client->socketd, (void *) buffer, read, 0);
+				send (client->socketd, 
+						(void*) buffer, 
+						read, 
+						0);
 			}
 		}
 	}
 }
 
-/**
+/*
  * This function will be called by libev when there is a connection
  * ready to be accepted.
  */
@@ -179,14 +196,17 @@ void accept_cb (struct ev_loop* loop, struct ev_io* watcher_, int revents)
 	}
 
 	/* Accept client request */
-	client_socket = accept (w->socketd, (struct sockaddr*) &client_addr, &client_len);
+	client_socket = accept (
+	                    w->socketd,
+	                    (struct sockaddr*) &client_addr,
+	                    &client_len);
 	if (INVALID_SOCKET == client_socket) {
 		socket_perror ("Accept error");
 		return;
 	}
 
 	/* We've accepted a new client, create a client object. */
-	client = (struct client_t *) calloc (1, sizeof (*client));
+	client = (struct client_t*) calloc (1, sizeof (*client));
 
 	if (client == NULL) {
 		perror ("malloc failed");
@@ -194,50 +214,54 @@ void accept_cb (struct ev_loop* loop, struct ev_io* watcher_, int revents)
 	}
 	client->socketd = client_socket;
 
-	// Initialize and start watcher to read client requests
-	ev_io_init (&client->io, read_cb, _open_osfhandle(client_socket, 0), EV_READ);
+	/* Initialize and start watcher to read client requests */
+	ev_io_init (
+	    &client->io,
+	    read_cb,
+	    _open_osfhandle (client_socket, 0),
+	    EV_READ);
 
 	/* Add the new client to the tailq. */
 	TAILQ_INSERT_TAIL (&client_tailq_head, client, entries);
 
 	ev_io_start (loop, &client->io);
 
-	printf ("Accepted connection from %s\n", inet_ntoa(client_addr.sin_addr));
+	printf ("Accepted connection from %s\n",
+	        inet_ntoa (client_addr.sin_addr));
 }
 
 int
-main(int argc, char **argv)
+main (int argc, char** argv)
 {
-	SOCKET listen_socket;
 	struct WSAData wsa_data;
 	u_long nonblock_mode = 1;
 	int reuseaddr_mode = 1;
 	struct sockaddr_in listen_addr;
+	SOCKET listen_socket;
 	int listen_fd;
 	struct ev_loop* loop;
-	struct server_t  w_accept;
+	struct server_t w_accept;
 
-	WSAStartup(MAKEWORD(2,2), &wsa_data);
+	WSAStartup (MAKEWORD (2, 2), &wsa_data);
 
 	/* Initialize the tailq. */
 	TAILQ_INIT (&client_tailq_head);
 
 	listen_socket = socket (AF_INET, SOCK_STREAM, 0);
 	if (INVALID_SOCKET == listen_socket) {
-		socket_perror("socket");
+		socket_perror ("socket");
 		return EXIT_FAILURE;
 	}
 
-	if (0 != ioctlsocket (listen_socket, FIONBIO, &nonblock_mode))
-	{
-		socket_perror("Ioctlsocket");
+	if (0 != ioctlsocket (listen_socket, FIONBIO, &nonblock_mode)) {
+		socket_perror ("ioctlsocket");
 		return EXIT_FAILURE;
 	}
 
 	if (0 != setsockopt (
-			listen_socket, SOL_SOCKET, SO_REUSEADDR,
-			(void*) &reuseaddr_mode, sizeof (reuseaddr_mode))) {
-		socket_perror("setsockopt");
+	        listen_socket, SOL_SOCKET, SO_REUSEADDR,
+	        (void*) &reuseaddr_mode, sizeof (reuseaddr_mode))) {
+		socket_perror ("setsockopt");
 		return EXIT_FAILURE;
 	}
 
@@ -247,8 +271,11 @@ main(int argc, char **argv)
 	listen_addr.sin_addr.s_addr = INADDR_ANY;
 
 	/* Bind socket to address */
-	if (0 != bind (listen_socket, (struct sockaddr*) &listen_addr, sizeof (listen_addr))) {
-		socket_perror("bind");
+	if (0 != bind (
+	        listen_socket,
+	        (struct sockaddr*) &listen_addr,
+	        sizeof (listen_addr))) {
+		socket_perror ("bind");
 		return EXIT_FAILURE;
 	}
 
@@ -258,11 +285,7 @@ main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
-	/* This is a bit of pain. libev operates on file descriptors which on real OS'es are just the same
-	as sockets but on windows this is not true. The windows socket can however be converted to
-	a fd so it can be used with libev. The original socket is stored in the watcher since it's needed
-	for sending/accepting. */
-	listen_fd = _open_osfhandle(listen_socket, 0);
+	listen_fd = _open_osfhandle (listen_socket, 0);
 	loop = ev_default_loop (0);
 
 	w_accept.socketd = listen_socket;
@@ -270,8 +293,8 @@ main(int argc, char **argv)
 	ev_io_init (&w_accept.io, accept_cb, listen_fd, EV_READ);
 	ev_io_start (loop, &w_accept.io);
 
-	// Start infinite loop
-	while (1) {
+	/* Start infinite loop */
+	for (;;) {
 		ev_loop (loop, 0);
 	}
 
